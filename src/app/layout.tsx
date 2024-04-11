94a7254f963edbea3f2c50b525acd0474a4e40e4
LaMuerte95 <juanemail2001@gmail.com>
2024-04-11 13:59:27 -0600

    jsut in case

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className} className="bg-orange-200">{children}</body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
